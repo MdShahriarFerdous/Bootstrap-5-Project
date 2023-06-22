c22fa42ec4cc830dc9f6750160291af10d25553d
Shahriar Ferdous <shahriarferdous2233@gmail.com>
2023-06-22 20:20:50 +0600

    bootstrap-5-project-V1

---
+++ index.js
@@ -0,0 +1,29 @@
+const currentTheme = localStorage.getItem("theme");
+
+document.documentElement.setAttribute("data-bs-theme", currentTheme || "light");
+
+const themeSwitchButton = document.getElementById("darkSwitch");
+themeSwitchButton.addEventListener("click", toggleTheme);
+
+function toggleTheme() {
+	const currentTheme = document.documentElement.getAttribute("data-bs-theme");
+
+	const newTheme = currentTheme === "light" ? "dark" : "light";
+	document.documentElement.setAttribute("data-bs-theme", newTheme);
+
+	localStorage.setItem("theme", newTheme);
+}
+
+// const htmlTag = document.documentElement;
+// const bodyTag = document.body;
+// const hasClass = htmlTag.classList.contains("dark");
+
+// const courseSection = document.querySelector("#course-section");
+// const heading = courseSection.querySelector("h3");
+// const paragraph = courseSection.querySelector("p");
+
+// if (hasClass) {
+// 	bodyTag.style.color = "lightblue";
+// 	heading.style.color = "white";
+// 	paragraph.style.color = "white";
+// }
